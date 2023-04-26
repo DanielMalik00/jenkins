839e12cc8099fc638a904a3502883196ae645edd
user <danimal889@student.polsl.pl>
2023-04-26 11:14:13 +0200

    Initial commit

---
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -0,0 +1,18 @@
+package com.example.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+	@GetMapping
+	public String get() {
+		return "Hello Jenkins!";
+	}
+}
